f424f82ed9ea032b234108fc4556683833c035dc
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-11-25 22:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Person {}
 
-class Student extends Person {}
+class Person {
+constructor(name,age)	{
+	this.name = name;
+	this.age = age;
+}
+	get name(){
+		return this.name
+	}
+	set age (){
+		return this.age
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name)
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(this.name)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
